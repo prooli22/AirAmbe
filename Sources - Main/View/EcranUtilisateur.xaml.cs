e182cb7179cf96241036e139ded88892440bd9a0
vincegame24 <vincegame24@hotmail.com>
2016-10-07 14:18:52 -0400

    Fin cours avant semaine de relache

--- Sources - Main/View/EcranUtilisateur.xaml.cs
+++ Sources - Main/View/EcranUtilisateur.xaml.cs
@@ -22,11 +22,13 @@ namespace AirAmbe
     public partial class EcranUtilisateur : Window
     {
         public string Retour { get; set; }
-        public Utilisateur UsagerActuel { get; set; }
+        public Utilisateur UsagerAfficher { get; set; }
         public EcranUtilisateur()
         {
             InitializeComponent();
 
+            InitialiseCbo();
+            SetTxtWritable(true);
             Retour = "EcranAdministrateur";
             btnAction.Content = "Ajouter";
             lblNouvMdp.Content = "Mot de passe: ";
@@ -35,12 +37,13 @@ namespace AirAmbe
         {
             InitializeComponent();
 
-            UsagerActuel = user;
-            lblTitre.Content = lblTitre.Content + ": " + user.NomUtilisateur;
+            InitialiseCbo();
+            UsagerAfficher = user;
+            lblTitre.Content = lblTitre.Content + " : " + user.NomUtilisateur;
             btnAction.Content = "Modifier";
             if (peutModifier)
             {
-                SetTxtWritable();
+                SetTxtWritable(false);
                 InitialiseValeur(user);
                 Retour = "EcranAdministrateur";
             }
@@ -50,6 +53,12 @@ namespace AirAmbe
             }
         }
 
+        private void InitialiseCbo()
+        {
+            cboType.Items.Add(Type.Administrateur.ToString());
+            cboType.Items.Add(Type.Contrôleur.ToString());
+        }
+
         private void InitialiseValeur(Utilisateur user)
         {
             txtAdresse.Text = user.Adresse;
@@ -57,12 +66,12 @@ namespace AirAmbe
             txtDate.Text = user.DateEmbauche.ToString("d");
             txtNom.Text = user.Nom;
             txtPrenom.Text = user.Prenom;
-            txtType.Text = user.TypeUtilisateur;
+            cboType.SelectedValue = user.TypeUtilisateur;
             txtPoste.Text = user.Poste;
             txtNum.Text = user.Telephone;
         }
 
-        private void SetTxtWritable()
+        private void SetTxtWritable(bool ajout)
         {
             txtAdresse.IsReadOnly = false;
             txtCour.IsReadOnly = false;
@@ -71,10 +80,13 @@ namespace AirAmbe
             txtNum.IsReadOnly = false;
             txtPoste.IsReadOnly = false;
             txtPrenom.IsReadOnly = false;
-            txtType.IsReadOnly = false;
+            if (ajout)
+            {
+                cboType.IsEnabled = true;
+            }
         }
 
-        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        private void QuitterPage()
         {
             Window win;
             switch (Retour)
@@ -84,11 +96,78 @@ namespace AirAmbe
                     win.Show();
                     break;
                 case "EcranControleur":
-                    win = new EcranControleur(UsagerActuel);
+                    win = new EcranControleur(UsagerAfficher);
                     win.Show();
                     break;
             }
             this.Close();
+        }
+
+        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        {
+            QuitterPage();
+        }
+
+        private void Modifier(Utilisateur user)
+        {
+            user.Adresse = txtAdresse.Text;
+            user.Courriel = txtCour.Text;
+            user.DateEmbauche = Convert.ToDateTime(txtDate.Text);
+            if (txtNouvMdp.Password != "" && txtNouvMdp.Password == txtConfMdp.Password)
+            {
+                user.MotPasse = MD5.Hash(txtNouvMdp.Password);
+            }
+            user.Nom = txtNom.Text;
+
+            user.NomUtilisateur = (txtPrenom.Text.Substring(0, 1) + txtNom.Text).ToLower();
+            user.Poste = txtPoste.Text;
+            user.Prenom = txtPrenom.Text;
+            user.Telephone = txtNum.Text;
+            user.TypeUtilisateur = cboType.SelectedValue.ToString();
+
+            UtilisateurAS userAs = new UtilisateurAS();
+            userAs.Modifier(user);
+        }
+
+        private void Ajouter()
+        {
+            Utilisateur user = new Utilisateur();
+
+            user.Adresse = txtAdresse.Text;
+            user.Courriel = txtCour.Text;
+            user.DateEmbauche = Convert.ToDateTime(txtDate.Text);
+            if (txtNouvMdp.Password == txtConfMdp.Password)
+            {
+                user.MotPasse = MD5.Hash(txtNouvMdp.Password);
+            }
+            user.Nom = txtNom.Text;
+            user.NomUtilisateur = (txtPrenom.Text.Substring(0, 1) + txtNom.Text).ToLower();
+            user.Poste = txtPoste.Text;
+            user.Prenom = txtPrenom.Text;
+            user.Telephone = txtNum.Text;
+            user.TypeUtilisateur = cboType.SelectedValue.ToString();
+
+            UtilisateurAS userAs = new UtilisateurAS();
+            userAs.Inserer(user);
+        }
+
+        private void btnAction_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult resultat = MessageBox.Show("Voulez-vous vraiment " + btnAction.Content.ToString().ToLower() + " l'utilisateur ?", "Quitter", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (resultat == MessageBoxResult.Yes)
+            {
+                if (btnAction.Content.ToString() == "Modifier")
+                {
+                    Modifier(UsagerAfficher);
+                    QuitterPage();                
+                }
+                else if (btnAction.Content.ToString() == "Ajouter")
+                {
+                    Ajouter();
+                    QuitterPage();
+                }
+            }
         }
     }
 }
